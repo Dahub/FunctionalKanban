820a33a214ccc978ea492ac3377dcc84dca257ab
David <dev.david.andre@gmail.com>
2021-04-03 18:24:58 +0200

    refacto wip

--- src/FunctionalKanban.Infrastructure/InMemory/InMemoryEntityStateRepository.cs
+++ src/FunctionalKanban.Infrastructure/InMemory/InMemoryEntityStateRepository.cs
@@ -1,6 +1,7 @@
 ﻿namespace FunctionalKanban.Infrastructure.InMemory
 {
     using System;
+    using System.Collections.Generic;
     using System.Linq;
     using System.Reflection;
     using FunctionalKanban.Domain.Common;
@@ -10,30 +11,47 @@
 
     public class InMemoryEntityStateRepository : IEntityStateRepository
     {
-        public Option<State> GetById(Guid id) 
+        public Option<State> GetById(Guid id) =>
+            GetEventLinesById(InMemoryDatabase.EventLines, id)
+                .Bind(WithEntityType)
+                .Bind(WithFromMethod)
+                .Bind(WithEvents)
+                .Bind(WithMethodInvocation);
+
+        private static Option<State> WithMethodInvocation((MethodInfo method, IEnumerable<Event> events) tuple) =>
+            (Option<State>)tuple.method.Invoke(null, new object[] { tuple.events });
+
+        private static Option<(MethodInfo method, IEnumerable<Event> lines)> WithEvents((MethodInfo method, IEnumerable<EventLine> lines) tuple) => 
+            Some((tuple.method, tuple.lines.Select(el => el.data)));
+
+        private static Option<(MethodInfo, IEnumerable<EventLine>)> WithFromMethod((Type entityType, IEnumerable<EventLine> lines) tuple)
         {
-            var eventsLines = InMemoryDatabase.EventLines.Where(e => e.aggregateId.Equals(id));
-            if(!eventsLines.Any())
-            {
-                return None;
-            }
-            var entityName = eventsLines.First().aggregateName;
-
-            var entityType = Assembly.GetAssembly(typeof(State)).GetType(entityName);
-            if(entityType == null)
-            {
-                return None;
-            }
-
-            var fromMethod = entityType.GetMethod("From");
+            var fromMethod = tuple.entityType.GetMethod("From");
             if (fromMethod == null)
             {
                 return None;
             }
 
-            var events = eventsLines.Select(el => el.data);
+            return Some((fromMethod, tuple.lines));
+        }
 
-            return (Option<State>)fromMethod.Invoke(null, new object[] { events });
+        private static Option<(Type, IEnumerable<EventLine>)> WithEntityType(IEnumerable<EventLine> lines)
+        {
+            var entityName = lines.First().aggregateName;
+            var entityType = Assembly.GetAssembly(typeof(State)).GetType(entityName);
+            if (entityType == null)
+            {
+                return None;
+            }
+            return Some((entityType, lines));
+        }
+
+        private static Option<IEnumerable<EventLine>> GetEventLinesById(IEnumerable<EventLine> allLines, Guid id)
+        {
+            var lines = allLines.Where(e => e.aggregateId.Equals(id));
+            return lines.Any()
+                ? Some(lines)
+                : None;
         }
     }
 }
